873a3a6b290d73838a85bf5057c9670253185d25
Chloe Hollingsworth <chollingsworth@pivotal.io>
2019-12-13 09:37:35 -0800

    Added redirect to 2.7 topic [#169140707]

--- redirects.rb
+++ redirects.rb
@@ -6,3 +6,4 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 # Redirects for docs-book-healthwatch
 
 r301 %r{/platform/application-service-windows/(?![\d-]+)(.*)}, "/platform/application-service-windows/2-8/$1"
+r301 %r'/platform/application-service-windows/2-8/upgrade-2012r2-to-pasw.html', '/platform/application-service-windows/2-7/upgrade-2012r2-to-pasw.html'
