8dc49bd0c40e2d6f43e5b118705b90caed970392
Chloe Hollingsworth <chollingsworth@pivotal.io>
2020-08-19 16:41:19 -0700

    [#174260277] Remove platform from Windows URL paths

--- redirects.rb
+++ redirects.rb
@@ -5,5 +5,8 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 
 # Redirects for docs-book-healthwatch
 
-r301 %r{/platform/application-service-windows/(?![\d-]+)(.*)}, "/platform/application-service-windows/2-9/$1"
-r301 %r'/platform/application-service-windows/2-8/upgrade-2012r2-to-pasw.html', '/platform/application-service-windows/2-7/upgrade-2012r2-to-pasw.html'
+r301 %r{/application-service-windows/(?![\d-]+)(.*)}, "/application-service-windows/2-9/$1"
+r301 %r'/application-service-windows/2-8/upgrade-2012r2-to-pasw.html', '/application-service-windows/2-7/upgrade-2012r2-to-pasw.html'
+
+r301 %r{/platform/application-service-windows/(?![\d-]+)(.*)}, "/application-service-windows/2-9/$1"
+r301 %r'/platform/application-service-windows/2-8/upgrade-2012r2-to-pasw.html', '/application-service-windows/2-7/upgrade-2012r2-to-pasw.html'
