5950474e66cf6afc9112ba8c84bdf919431dbe8d
Chloe Hollingsworth <chollingsworth@pivotal.io>
2020-08-19 14:42:25 -0700

    [#174260277] Correct version number

--- redirects.rb
+++ redirects.rb
@@ -5,8 +5,8 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 
 # Redirects for docs-book-healthwatch
 
-r301 %r{/application-service-windows/(?![\d-]+)(.*)}, "/application-service-windows/2-10/$1"
+r301 %r{/application-service-windows/(?![\d-]+)(.*)}, "/application-service-windows/2-11/$1"
 r301 %r'/application-service-windows/2-8/upgrade-2012r2-to-pasw.html', '/application-service-windows/2-7/upgrade-2012r2-to-pasw.html'
 
-r301 %r{/platform/application-service-windows/(?![\d-]+)(.*)}, "/application-service-windows/2-10/$1"
+r301 %r{/platform/application-service-windows/(?![\d-]+)(.*)}, "/application-service-windows/2-11/$1"
 r301 %r'/platform/application-service-windows/2-8/upgrade-2012r2-to-pasw.html', '/application-service-windows/2-7/upgrade-2012r2-to-pasw.html'
