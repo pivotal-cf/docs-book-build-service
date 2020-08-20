2d17c80848bd1f01c308cbe7f822c24d37027f3b
Chloe Hollingsworth <chollingsworth@pivotal.io>
2020-08-20 12:30:06 -0700

    [#174260277] Added redirect

--- redirects.rb
+++ redirects.rb
@@ -10,3 +10,6 @@ r301 %r'/application-service-windows/2-8/upgrade-2012r2-to-pasw.html', '/applica
 
 r301 %r{/platform/application-service-windows/(?![\d-]+)(.*)}, "/application-service-windows/2-11/$1"
 r301 %r'/platform/application-service-windows/2-8/upgrade-2012r2-to-pasw.html', '/application-service-windows/2-7/upgrade-2012r2-to-pasw.html'
+
+# Redirects from platform/application-service-windows to /application-service-windows
+r301 %r{/platform/application-service-windows/2-11/(.*)}, '/application-service-windows/2-11/$1'
