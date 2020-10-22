6955ebc9b60f7b78c459fed0b3a30206e4e9e692
Chloe Hollingsworth <chollingsworth@pivotal.io>
2020-10-21 19:49:52 -0700

    [#174604562] Reordered redirect

--- redirects.rb
+++ redirects.rb
@@ -3,6 +3,9 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
+# Redirects from platform/application-service-windows to /application-service-windows
+r301 %r{/platform/application-service-windows/2-11/(.*)}, '/application-service-windows/2-11/$1'
+
 # Redirects for docs-book-healthwatch
 
 r301 %r{/application-service-windows/(?![\d-]+)(.*)}, "/application-service-windows/2-10/$1"
@@ -10,6 +13,3 @@ r301 %r'/application-service-windows/2-8/upgrade-2012r2-to-pasw.html', '/applica
 
 r301 %r{/platform/application-service-windows/(?![\d-]+)(.*)}, "/application-service-windows/2-10/$1"
 r301 %r'/platform/application-service-windows/2-8/upgrade-2012r2-to-pasw.html', '/application-service-windows/2-7/upgrade-2012r2-to-pasw.html'
-
-# Redirects from platform/application-service-windows to /application-service-windows
-r301 %r{/platform/application-service-windows/2-11/(.*)}, '/application-service-windows/2-11/$1'
