2e7f90430529fcbfe26a547acb0f7430a6f0355a
Melinda Gutermuth <mgutermuth@vmware.com>
2022-01-27 10:41:25 -0800

    [#180485456] Add redirect for new URL

--- redirects.rb
+++ redirects.rb
@@ -4,12 +4,15 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 }
 
 # Redirects from platform/application-service-windows to /application-service-windows
-r301 %r{/platform/application-service-windows/2-12/(.*)}, '/application-service-windows/2-12/$1'
+r301 %r{/platform/application-service-windows/2-12/(.*)}, '/application-service/2-12/windows/$1'
+
+# Redirects from application-service-windows to application-service/windows
+r301 %r{/application-service-windows/2-12/(.*)}, '/application-service/2-12/windows/$1'
 
 # Redirects for docs-book-healthwatch
 
-r301 %r{/application-service-windows/(?![\d-]+)(.*)}, "/application-service-windows/2-11/$1"
-r301 %r'/application-service-windows/2-8/upgrade-2012r2-to-pasw.html', '/application-service-windows/2-7/upgrade-2012r2-to-pasw.html'
+r301 %r{/application-service-windows/(?![\d-]+)(.*)}, "/application-service/2-11/windows/$1"
+r301 %r'/application-service-windows/2-8/upgrade-2012r2-to-pasw.html', '/application-service/2-7/upgrade-2012r2-to-pasw.html'
 
 r301 %r{/platform/application-service-windows/(?![\d-]+)(.*)}, "/application-service-windows/2-1/$1"
-r301 %r'/platform/application-service-windows/2-8/upgrade-2012r2-to-pasw.html', '/application-service-windows/2-7/upgrade-2012r2-to-pasw.html'
+r301 %r'/platform/application-service-windows/2-8/upgrade-2012r2-to-pasw.html', '/application-service/2-7/windows/upgrade-2012r2-to-pasw.html'
